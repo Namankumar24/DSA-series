0326b795f69b58f9d95a277a96a01eafc2386e34
Naman Kumar <namankumar24022007@gmail.com>
2026-01-03 17:51:13 +0530

    even number

---
+++ arrayeven.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " numbers:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Even numbers are: ";
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0;
+}
