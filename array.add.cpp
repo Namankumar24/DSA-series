2702ffa8dfaa625c80644ea0bfe02252135ef373
Naman Kumar <namankumar24022007@gmail.com>
2026-01-06 20:24:52 +0530

    Addition of two matrices

---
+++ array.add.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int row, col ;
+
+    cout << "Enter the no. of rows & cols. of the matrix..: ";
+    cin >> row>>col;
+
+    int matrix1[row][col], matrix2[row][col], sum[row][col];
+    cout <<"Enter the elements of 1st matrix..: ";
+
+    for(int i=0; i< row; i++){
+        for(int j=0; j< col; j++){
+            cin >> matrix1[i][j];
+        }
+    }
+
+    cout<<" Enter the elements of 2nd matrix..: ";
+
+    for (int i=0; i< row; i++){ 
+        for (int j=0; j< col; j++){
+            cin>> matrix2[i][j];
+        }
+    }
+    
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            sum[i][j]= matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    
+    cout<<" Sum of the two matrixs are..: "<< endl;
+       for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
