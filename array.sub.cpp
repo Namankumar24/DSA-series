07e3153b88f6b084974399bbe9414e92c34caffd
Naman Kumar <namankumar24022007@gmail.com>
2026-01-07 19:49:06 +0530

    Sub two matrix using array

---
+++ array.sub.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     
+    int row,col;
+
+    cout <<"Enter the no. of rows and cols. of the matrix..: ";
+    cin>> row>>col;
+
+    int matrix1[row][col], matrix2[row][col], sum[row][col];
+    cout<<"Enter the elements of the 1st matrix..: "<<endl;
+
+    for(int i= 0; i< row; i++){
+        for(int j= 0; j< col; j++){
+            cin>> matrix1[i][j];
+        }
+    }
+
+    cout<<" Enter the elements of the 2nd matrix..: "<<endl;
+
+    for(int i= 0; i< row; i++){
+        for(int j= 0; j< col; j++){
+            cin>> matrix2[i][j];
+        }
+    }
+    
+    for(int i=0; i<row; i++){
+        for(int j=0; j< col; j++){
+            sum[i][j]= matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    cout<<"Sum of two matrices are.. : "<< endl;
+
+    for(int i= 0; i< row; i++){
+        for(int j=0; j< col; j++){
+            cout<< sum[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
