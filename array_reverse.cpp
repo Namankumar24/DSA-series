faf3ff07895046f8606b218a8bdf3eef1928b5e9
Naman Kumar <namankumar24022007@gmail.com>
2026-01-16 20:08:52 +0530

    Basics of c++

---
+++ array_reverse.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() { 
+
+    int n;
+
+    cout<<"Enter size of the Array :- "<<endl;
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter Array elements :- \n ";
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n/2;i++) {
+        int temp=arr[i];
+        arr[i]= arr[n-1-i];
+        arr[n-1-i]=temp;
+    }
+
+    cout<<"Reversed Array :-\n";
+    for(int i=0;i<n;i++) {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+    
+}
