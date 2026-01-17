5ee1f14ba31c0948415e68d0ded008ccaa64b81d
Naman Kumar <namankumar24022007@gmail.com>
2026-01-17 12:59:35 +0530

    Basics of Strings in c++

---
+++ string_print_line.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    string line;
+    cout<<"Enter the sentence : \n";
+    getline(cin, line);
+
+    cout <<line;
+
+    return 0;
+}
