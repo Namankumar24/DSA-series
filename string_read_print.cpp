3c0f9b45340c00ee001c7a8a49a3f00d20dea0d1
Naman Kumar <namankumar24022007@gmail.com>
2026-01-17 12:49:45 +0530

    Basics of Strings in c++

---
+++ string_read_print.cpp
@@ -0,0 +1,14 @@
+ 
+ #include<iostream>
+using namespace std;
+
+int main() {
+
+    string name ;       
+
+    cout<<"Enter Name : ";
+    cin>>name;
+
+    cout<<"Nice to meet you "<< name;
+    return 0;
+}
