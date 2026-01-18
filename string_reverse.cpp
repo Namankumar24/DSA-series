bbb1f62476f7d33693b653fc392a8ff527dea042
Naman Kumar <namankumar24022007@gmail.com>
2026-01-18 19:55:27 +0530

    Basics of Strings in c++

---
+++ string_reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    string s;
+
+    cout<<"Enter word or sentence :-\n";
+    getline(cin,s);
+
+    for(int i= s.length() -1 ; i>=0; i--)
+
+    cout<<s[i];
+    return 0;
+}
