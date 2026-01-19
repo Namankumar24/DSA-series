1dd180bf302d3ff59e834dab3e1f4e383587c653
Naman Kumar <namankumar24022007@gmail.com>
2026-01-19 23:12:33 +0530

    Basics of String in c++

---
+++ string_to_uppercase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main () {
+
+    string s;
+
+    cout<<"Enter String :- \n";
+    cin>>s;
+
+    for(int i = 0; i < s.length(); i++) {
+
+        if (s[i]>= 'a' && s[i]<= 'z') 
+            s[i] = s[i]-32;
+
+    }
+
+    cout<<s;
+    return 0;
+}
